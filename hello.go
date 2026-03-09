db3477123e36350543cf86a986411e9d11676c15
simonf <simoncfarrow@gmail.com>
2026-03-09 14:58:43 +0000

    Comments

--- hello.go
+++ hello.go
@@ -7,6 +7,8 @@ import (
 	"strconv"
 )
 
+// Note the lack of semicolons, they are optional in Go and are automatically inserted by the compiler at the end of lines,
+// but they can be used to separate multiple statements on the same line if needed.
 // ====================================================================================================================
 func main() {
 	for i := range 10 {
@@ -99,6 +101,9 @@ type Dog struct {
 	Breed string
 }
 
+// receiver functions, they are like methods in other languages, but they are not associated with the type in the same way as methods in other languages, 
+// they are just functions that take a receiver as the first argument. The receiver can be a value or a pointer, and it can be of any type, not just structs. 
+// The receiver is specified in parentheses before the function name, and it can be used to access the fields of the struct or to modify the struct if it is a pointer receiver.
 func (u *User) Greet() {
 	fmt.Println("Hello from", u.Name)
 }
